9dddcd893f81703a30583cd6ea444a5e31d35370
dhanahere <dhanalekshmi86@gmail.com>
2018-07-16 23:19:34 +0530

    Adding dependencies for Hibernate..started working on Hibernate connectivity in LoginDao.java

--- Annapoorna/src/main/java/com/dhana/Annapoorna/dao/LoginDao.java
+++ Annapoorna/src/main/java/com/dhana/Annapoorna/dao/LoginDao.java
@@ -5,25 +5,31 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.Random;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 import com.dhana.Annapoorna.entity.Login;
 
 @Component
 public class LoginDao {
+	
+	private SessionFactory sessionFactory;
 
 	public void checkLogin(Login login) {
 		
 		System.out.println("==inside login=="+login);
 		
-		String url="jdbc:mysql://localhost:3306/annapoorna";
+		/*String url="jdbc:mysql://localhost:3306/annapoorna";
 		String uName="root";
 		String pass="root";
 		
 		String userName=login.getUsername();
-		String password=login.getPassword();
+		String password=login.getPassword();*/
 		
-		try
+		//JDBC Connection
+		
+		/*try
 		{				
 			//String query="select * from student";
 			String query="insert into userRegistration values(?,?,?)";
@@ -34,11 +40,11 @@ public class LoginDao {
 			st.setInt(1,1);
 			st.setString(2, userName);
 			st.setString(3, password);
-			/*ResultSet rs=st.executeQuery(query);
-			while(rs.next())
-			{
-				System.out.println(rs.getInt(1)+"******"+rs.getString(2));
-			}*/
+			//ResultSet rs=st.executeQuery(query);
+			//while(rs.next())
+			//{
+				//System.out.println(rs.getInt(1)+"******"+rs.getString(2));
+			//}
 			int count=st.executeUpdate();
 			System.out.println("==rows affected==="+count);
 			st.close();
@@ -47,7 +53,14 @@ public class LoginDao {
 		catch(Exception e)
 		{
 			System.out.println("Exception e===="+e);
-		}
+		}*/
+		
+		//HiberNate
+		
+		
+		Session session=sessionFactory.getCurrentSession();
+		session.save(login);
+		
 	}
 
 }
